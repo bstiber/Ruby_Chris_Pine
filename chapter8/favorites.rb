de6bba5eebb215ca43596fd494932d03612613b6
Bill Stiber <billstiber@gmail.com>
2016-06-12 14:44:05 -0400

    add file and exercises

---
+++ chapter8/favorites.rb
@@ -0,0 +1,12 @@
+favorites = []
+
+favorites.push("eraser")
+favorites.push "time traveler"
+
+puts favorites.length
+puts favorites.last
+puts favorites[0]
+
+puts favorites.pop
+puts favorites
+puts favorites.length
--- chapter8/language.rb
+++ chapter8/language.rb
@@ -3,3 +3,8 @@ languages = ["spanish", 'pig latin', 'taga\'log']
 languages.each do |language|
  puts " I love " + language + " don\'t you?"
 end
+
+
+3.times do
+    puts "hip hip hooray"
+end
