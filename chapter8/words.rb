ab42f028ef18ca718c4da74cf81b722506d1432c
Bill Stiber <billstiber@gmail.com>
2016-06-12 15:48:17 -0400

    add file words

--- chapter8/words.rb
+++ chapter8/words.rb
@@ -1,9 +1,17 @@
 # words.rb
 
- puts "enter some words and press enter when you are done"
- input = gets.chomp
- words = []
+puts "enter some words and press enter when you are done"
+input = gets.chomp
+words = []
 
- words.push(input)
-puts "these are the words that you entered into an arry: #{words}.sort"
+words.push(input)
+puts "these are the words that you entered into an arry: #{words}"
+words.join(',')
 puts words
+
+# words = ['bob','john', 'cow']
+#
+# words.sort do |x, y|
+#    words
+# end
+# puts words
