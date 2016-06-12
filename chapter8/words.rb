fcf5ed34bfa789beb46d98973c6dccdef71f51b6
Bill Stiber <billstiber@gmail.com>
2016-06-12 14:59:20 -0400

    add file words.rb

---
+++ chapter8/words.rb
@@ -0,0 +1,9 @@
+# words.rb
+
+ puts "enter some words and press enter when you are done"
+ input = gets.chomp
+ words = []
+
+ words.push(input)
+puts "these are the words that you entered into an arry: #{words}"
+puts words
