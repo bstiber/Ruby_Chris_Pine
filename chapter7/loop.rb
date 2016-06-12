b0d353e83d79b604d4b671e85da366de4e881898
Bill Stiber <billstiber@gmail.com>
2016-06-12 11:52:16 -0400

    add file loop.rb

---
+++ chapter7/loop.rb
@@ -0,0 +1,10 @@
+puts "what is your name?"
+name = gets.chomp
+puts "hello " + name
+if name == "Bill"
+  puts "thats the best name ever"
+else
+  if name == "kate"
+  puts "hi " + name + " how are you?"
+end
+end
