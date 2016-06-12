767da7a3e59a8f683a7c6ca64ce7296ef2cb2c45
Bill Stiber <billstiber@gmail.com>
2016-06-12 19:08:40 -0400

    add file moo.rb, 'first def_method'

---
+++ chapter9/moo.rb
@@ -0,0 +1,19 @@
+def say
+puts "moooo"
+end
+
+puts say
+puts say
+puts "----------"
+puts "coin coin"
+puts "----------"
+puts say
+puts say
+
+
+def say(number_of_moos)
+puts "moooo... " * number_of_moos
+end
+
+puts say(3)
+puts " oink oink "
