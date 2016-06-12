5d1bada06a7c7401bb28ffd17759a6d5387ed395
Bill Stiber <billstiber@gmail.com>
2016-06-12 16:09:49 -0400

    add 'exercise solution

---
+++ chapter6/centering_code.rb
@@ -0,0 +1,9 @@
+line_width = 55
+
+puts "Table of Contents".center(line_width)
+puts
+puts "Chapter 1         Getting started       Page 1".ljust(line_width)
+puts "Chapter 2         Numbers               Page 9".ljust(line_width)
+puts "Chapter 3         Letter                Page 13".ljust(line_width)
+puts
+puts "by Bill Stiber".rjust(line_width)
