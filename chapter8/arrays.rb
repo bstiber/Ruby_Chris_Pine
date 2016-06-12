992d19a3ba6b155e0efae4f6840f27312ded9fa0
Bill Stiber <billstiber@gmail.com>
2016-06-12 14:21:02 -0400

    add file arrays, chris pine practice

---
+++ chapter8/arrays.rb
@@ -0,0 +1,10 @@
+# arrays.rb
+
+other_peeps = []
+other_peeps[0] = 'who cares'
+other_peeps[3] = 'bla blah'
+other_peeps[1] = 'another something'
+other_peeps[2] = []
+other_peeps[4] = 'name'
+
+puts other_peeps
